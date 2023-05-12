fbc05cdbcde0b6c97fea317efdcf34090c9e93fa
wallace <wallace_wolv@hotmail.com>
2023-05-12 16:32:21 -0300

    feat: Organizando melhor nosso código

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -11,14 +11,17 @@ export class NegociacaoController {
     this.inputValor = document.querySelector("#valor");
   }
 
-  adiciona() {
+  adiciona(): void {
+    const negociacao = this.criarNegociacao();
+    console.log(negociacao);
+  }
+
+  criarNegociacao(): Negociacao {
     const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ","));
+    const data = new Date(this.inputData.value.replace(exp, ","));
     const quantidade = parseInt(this.inputQuantidade.value);
     const valor = parseInt(this.inputValor.value);
-    
-    const negociacao = new Negociacao(date, quantidade, valor);
 
-    console.log(negociacao);
+    return new Negociacao(data, quantidade, valor);
   }
 }
